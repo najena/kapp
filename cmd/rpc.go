67856a530d1a522c94433607b865f52dc4116147
Andrew Wittrock <awittrock@gmail.com>
2018-01-02 17:39:41 -0800

    Add rpc command that reads Pods using client-go

--- cmd/rpc.go
+++ cmd/rpc.go
@@ -15,28 +15,30 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/spf13/cobra"
-)
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	)
 
 // rpcCmd represents the rpc command
-var rpcCmd = &cobra.Command{
-	Use:   "rpc",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rpc called")
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(rpcCmd)
+	rpcCmd := &RpcCommand{}
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "rpc",
+		Short: "",
+		Long: ``,
+		Run: rpcCmd.Run,
+	})
 
 	// Here you will define your flags and configuration settings.
 
@@ -47,4 +49,61 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// rpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
+type RpcCommand struct {}
+
+func (r *RpcCommand) Run(cmd *cobra.Command, args []string) {
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		namespace := "default"
+		pod := "example-xxxxx"
+		_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				pod, namespace, statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
